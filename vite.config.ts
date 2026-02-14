596ce5a51d1fe14f0fc81782c8c953a209837af0
MonsterXia <monster1018629681@gmail.com>
2026-02-14 23:28:15 +0800

    edit localhost for cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,5 +13,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    host: 'localhost.246801357.xyz',
+    allowedHosts: [
+      'localhost.246801357.xyz',
+    ],
   },
 })
